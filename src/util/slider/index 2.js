48a14dff3dbafbbfe86be41bb39bdb727f4bfcee
quanxin <1307683181@qq.com>
2020-07-11 17:35:07 +0800

    list error

---
+++ src/util/slider/index 2.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Huracan
+* @Date:   2020-02-24 18:24:19
+* @Last Modified by:   Huracan
+* @Last Modified time: 2020-02-24 18:25:53
+*/
+'use strict';
+
+require('./index.css');
+require('./unslider.js');
