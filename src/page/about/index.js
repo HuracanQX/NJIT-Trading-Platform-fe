d5f68f5c10b5be1ac99f6f99296eaad1e9a1bb17
quanxin <1307683181@qq.com>
2020-09-20 08:16:22 +0800

    about edit

--- src/page/about/index.js
+++ src/page/about/index.js
@@ -8,3 +8,21 @@
 
 require('page/common/header/index.js');
 require('page/common/nav/index.js');
+var navSide         = require('page/common/nav-side/index.js');
+// page 逻辑部分
+var page = {
+    init: function(){
+        this.onLoad();
+    },
+    onLoad : function(){
+        // 初始化左侧菜单
+        navSide.init({
+            name: 'about'
+        });
+        // 加载用户信息
+        this.loadUserInfo();
+    }
+};
+$(function(){
+    page.init();
+});
